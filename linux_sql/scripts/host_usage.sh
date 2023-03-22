8b1adb223570cdd9ed35a3eaaf7d556d74b27848
miharbii <fradj.ibrahim@gmail.com>
2023-03-22 22:09:12 +0000

    fix some errors

--- linux_sql/scripts/host_usage.sh
+++ linux_sql/scripts/host_usage.sh
@@ -15,16 +15,17 @@ vmstat_mb=$(vmstat --unit M)
 hostname=$(hostname -f)
 
 memory_free=$(echo "$vmstat_mb" | awk '{print $4}'| tail -n1 | xargs)
-cpu_idle=$(echo "$vmstat_mb" | tail -1 | awk '{print $15}' | xargs)
+cpu_idel=$(echo "$vmstat_mb" | tail -1 | awk '{print $15}' | xargs)
 cpu_kernel=$(echo "$vmstat_mb" | tail -1 | awk -v col="14" '{print $col}' | xargs)
 disk_io=$(vmstat -d | tail -1 | awk -v col="10" '{print $col}' | xargs)
 disk_available=$(df -m / | awk 'NR==2{print $4}' | xargs)
 timestamp=$(date +"%Y-%m-%d %H:%M:%S")
 
-host_id="(SELECT id FROM host_info WHERE hostname='$hostname')";
+host_id=$(psql -h localhost -p 5432 -U postgres -d host_agent -c "SELECT id FROM host_info WHERE hostname='$hostname'" | sed -n "3p" | xargs)
 
-insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idle, cpu_kernel, disk_io, disk_available)
-VALUES('$timestamp','$host_id','$memory_free','$cpu_idle', '$cpu_kernel','$disk_io','$disk_available')"
+
+insert_stmt="INSERT INTO host_usage(timestamp, host_id, memory_free, cpu_idel, cpu_kernel, disk_io, disk_available)
+VALUES('$timestamp','$host_id','$memory_free','$cpu_idel','$cpu_kernel','$disk_io','$disk_available')"
 
 #set up env var for pql cmd
 export PGPASSWORD=$psql_password 
